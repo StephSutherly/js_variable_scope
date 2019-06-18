6b82e84d466e925083d1c797671f35eef50691cb
Steph Scully <stephanielarascullion@yahoo.com>
2019-06-18 11:02:54 +0100

    Monday homework file added

---
+++ homework.js
@@ -0,0 +1,209 @@
+// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why. Copy the following episodes into a JavaScript file and add comments under each one detailing the reason for your predicted output.
+//
+// Episode 1
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+//It will log 'The murderer is Miss Scarlet' (Correct)
+
+// Episode 2
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// It will throw an error - can't change constant. (Correct)
+
+// Episode 3
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+// It will log 'Mrs Peacock' - inside the function, overrides the outside variable. (Correct)
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// It will log 'Professor Plum' - outside the function, doesn't know about variables within. (Correct)
+
+// Episode 4
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// It will log 'The suspects are Miss Scarlet, Professor Plum, Colonel Mustard'. (Correct)
+// It will log 'Suspect three is Mrs Peacock' - outside of function, suspectThree is not reassigned but original. (Correct)
+
+// Episode 5
+//
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// It will log 'The weapon is the Revolver'. The scenario is the constant but you can 'legally' change key/value pairs within. (Correct)
+
+// Episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Murderer is Colonel Mustard then after plotTwist is Mrs. White. After changeMurderer it will log 'The murderer is Mr. Green' (INCORRECT - not 100% sure why)
+//
+// Episode 7
+//
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Murderer is Prof Plum, after unexpectedOutcome is Miss Scarlet, after plotTwist is Colonel Mustard, after changeMurderer, it will log 'The murderer is Mr. Green' (Correct)
+
+// Episode 8
+
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+// To begin: Peacock, Conservatory, Lead Pipe. unexpectedOutcome changes nothing (if=false). plotTwist changes nothing (if=false). changeScenario: Peacock, Dining Room, Lead Pipe. It will log, 'The weapon is Lead Pipe'. (incorrect - not 100% sure why)
+
+// Episode 9
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// It will log 'The murderer is Mrs Peacock' (incorrect - not 100% sure why)
+//
+// // Extensions
+// // Make up your own episode!
